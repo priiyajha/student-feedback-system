daa98fefc8373e246a1b3428249f782a9eed1a76
priiyajha <jhapriiyaa2104@gmail.com>
2025-10-05 12:33:31 +0530

    Basic home pg ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <div>Page</div>
+        <>
+            <section className="card-cta flex flex-col justify-center items-center">
+                <div className=" gap-3 max-w mb-20" >
+                    <h2>Check and Submit Ratings and Reviews</h2>
+                    <p className="text-xl text-center my-10">
+                        See real feedbacks & get instant insights
+                    </p>
+
+                </div>
+
+            {/*</section>*/}
+
+            {/*<section className="flex flex-col gap-6 mt-8">*/}
+            {/*    <h2>Your Interviews</h2>*/}
+
+
+            {/*</section>*/}
+
+            {/*<section className="flex flex-col gap-6 mt-8">*/}
+            {/*    <h2>Take Interviews</h2>*/}
+
+            </section>
+        </>
     )
 }
 export default Page
