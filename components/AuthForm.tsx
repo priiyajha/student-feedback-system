7ae70757e06eba4d253b3866642a53f88d0908b7
priiyajha <jhapriiyaa2104@gmail.com>
2025-10-05 12:45:06 +0530

    centered cards

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -52,65 +52,66 @@ const AuthForm = ({type}:{type : FormType}) => {
     const isSignIn = type ==='sign-in';
 
     return (
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="card-border lg:min-w-[566px]">
+                    <div className="flex flex-col gap-6 card py-14 px-10">
+                        <div className="flex flex-row gap-2 justify-center">
+                            <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                            <h2 className="text-primary-100">Student Feedback System</h2>
+                        </div>
 
-            <div className="card-border lg:min-w-[566px]">
-                <div className="flex flex-col gap-6 card py-14 px-10">
-
-                    <div className="flex flex-row gap-2 justify-center">
-                        <Image src="/logo.svg" alt="logo" height={32} width={38} />
-                        <h2 className="text-primary-100">Student Feedback System</h2>
-                    </div>
-
-                    <h3>Check and submit reviews and feedbacks.</h3>
+                        <h3>Check and submit reviews and feedbacks.</h3>
 
 
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)}
-                              className="w-full space-y-6 mt-4 form">
-                            {!isSignIn && (
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)}
+                                  className="w-full space-y-6 mt-4 form">
+                                {!isSignIn && (
+                                    <FormField
+                                        control={form.control}
+                                        name="name"
+                                        label="Name"
+                                        placeholder="Your Name"
+                                        type="text"
+                                    />
+                                )}
+
                                 <FormField
                                     control={form.control}
-                                    name="name"
-                                    label="Name"
-                                    placeholder="Your Name"
-                                    type="text"
+                                    name="email"
+                                    label="Email"
+                                    placeholder="Your email address"
+                                    type="email"
                                 />
-                            )}
 
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                label="Email"
-                                placeholder="Your email address"
-                                type="email"
-                            />
-
-                            <FormField
-                                control={form.control}
-                                name="password"
-                                label="Password"
-                                placeholder="Enter your password"
-                                type="password"
-                            />
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    label="Password"
+                                    placeholder="Enter your password"
+                                    type="password"
+                                />
 
 
-                            <Button className="btn" type="submit">
-                                {isSignIn ? "Sign In" : "Create an Account"}
-                            </Button>
-                        </form>
-                    </Form>
+                                <Button className="btn" type="submit">
+                                    {isSignIn ? "Sign In" : "Create an Account"}
+                                </Button>
+                            </form>
+                        </Form>
 
-                    <p className="text-center">
-                        {isSignIn ? "No account yet?" : "Have an account already?"}
-                        <Link
-                            href={!isSignIn ? "/sign-up" : "/sign-in"}
-                            className="font-bold text-user-primary ml-1"
-                        >
-                            {!isSignIn ? "Sign In" : "Sign Up"}
-                        </Link>
-                    </p>
+                        <p className="text-center">
+                            {isSignIn ? "No account yet?" : "Have an account already?"}
+                            <Link
+                                href={!isSignIn ? "/sign-up" : "/sign-in"}
+                                className="font-bold text-user-primary ml-1"
+                            >
+                                {!isSignIn ? "Sign In" : "Sign Up"}
+                            </Link>
+                        </p>
+                    </div>
                 </div>
             </div>
+
     )
 }
 export default AuthForm
